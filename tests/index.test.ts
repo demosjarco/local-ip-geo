8e93b7427c238ad0ee8f1776b48acfdaccdfb213
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2023-03-08 10:05:14 -0700

    Move tests from vitest to node:test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,18 +1,19 @@
 import { unstable_dev } from 'wrangler';
 import type { UnstableDevWorker } from 'wrangler';
-import { describe, expect, it, beforeAll, afterAll } from 'vitest';
+import { after, before, describe, it } from 'node:test';
+import { strict as assert } from 'node:assert';
 
 describe('Worker', () => {
 	let worker: UnstableDevWorker;
 
-	beforeAll(async () => {
+	before(async () => {
 		worker = await unstable_dev('src/index.ts', {
 			local: false,
 			experimental: { disableExperimentalWarning: true },
 		});
 	});
 
-	afterAll(async () => {
+	after(async () => {
 		await worker.stop();
 	});
 
@@ -21,10 +22,10 @@ describe('Worker', () => {
 		if (resp) {
 			const json = await resp.json();
 
-			expect(json).toHaveProperty('lat');
-			expect(json.lat).toBeTypeOf('number');
-			expect(json).toHaveProperty('long');
-			expect(json.long).toBeTypeOf('number');
+			assert.deepStrictEqual(Object.keys(json).includes('lat'), true);
+			assert.deepStrictEqual(typeof json.lat === 'number', true);
+			assert.deepStrictEqual(Object.keys(json).includes('long'), true);
+			assert.deepStrictEqual(typeof json.long === 'number', true);
 		}
 	});
 });
