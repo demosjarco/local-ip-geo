8e00dc457124ae71a0d64de4fc8893b7e73ee631
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2023-03-10 11:11:00 -0700

    Debug

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,16 @@ export interface Env {
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		let tempCf:Record<string,string> = {};
+		for (const index in Object.keys(request.cf)) {
+			tempCf[Object.keys(request.cf)[index]] = JSON.stringify(Object.values(request.cf)[index], null, '\t');
+		}
 		return new Response(
 			JSON.stringify(
 				{
 					lat: Number(request.cf.latitude) || Number(request.headers.get('cf-iplatitude')),
 					long: Number(request.cf.longitude) || Number(request.headers.get('cf-iplongitude')),
+					cf: tempCf,
 				},
 				null,
 				process.env.NODE_ENV === 'development' ? '\t' : undefined,
